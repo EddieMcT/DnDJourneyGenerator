f61697a21f05c33de986b216142be5ee18936a43
EddieMcT <62473774+EddieMcT@users.noreply.github.com>
2024-06-16 22:44:22 +0200

    Update eventgen.py

--- eventgen.py
+++ eventgen.py
@@ -41,12 +41,17 @@ def text_construct(input = test_dict, text = '[0]'):
 
 
 
-ruindict = { #General rule: use [n] as a grammatical placeholder for a/an, and replace after generation is complete. Needs to be done after the main construction function
-    0: ('The path goes [path_location] a[n] [description] [building].', 'The path goes [path_location] a[n] [building] [post_description].', 
-            "A[n] [description] [building] is [location]."),
+structuredict = { #General rule: use a[n] as a grammatical placeholder for a/an, and replace after generation is complete. Needs to be done after the main construction function
+    0: ('The path goes [path_location] a[n] [description] [structure].', 'The path goes [path_location] a[n] [structure] [post_description].', 
+            'The path goes [path_location] a[n] [description] [structure], long since [ruin_description].'
+            ,"A[n] [description] [structure] is [location].","A[n] [structure], visibly [ruin_description], is [location]."),
+    'structure' : ('[building]', '[monument]', '[shop]', '[settlement]'), #old: (specific_ruins(colours),random_ruins(),monument(),signpost(DC),shop(),village())
     'description' : ('','[basic_description]', '[ruin_description]'),
-    'ruin_description': ("abandoned", "overgrown","decayed","ancient", "ruined"),
-    'basic_description': (),
+    'ruin_description': ("abandoned", "overgrown","decayed","ruined", "plundered"),
+    'basic_description': ('great', "ancient", 'stone'),
+    'post_description': ('etched with [writing]', 'with [writing] carved into it', 'made from [material]' ),
+    'writing': ("a warning","a blessing","historical lore","a memorial","religious iconography","a holy symbol","an unholy symbol","arcane symbols","ancient graffiti", "a riddle"),
+    'surroundings' : ("fresh flowers","a strange smell", "mist", 'unusual plants', 'buzzing insects'),
     'path_location': ("through","past","next to","around"),
     'location': ("up ahead","just off the path","visible nearby",
                 "a short walk away from the path","a half-mile off the path"),
@@ -57,38 +62,22 @@ ruindict = { #General rule: use [n] as a grammatical placeholder for a/an, and r
     'int_location': ('study', 'library', 'tower'),
     'renown': ('famous', 'infamous', 'well-renowned', 'lesser known'),
     'faction': ('criminal club', 'guild', 'secret society', 'enemy faction', 'social club'), 
-    'monuments': ("sealed burial mound","plundered burial mound",
+    'material': ('native rock', 'assorted stones', 'imported marble'),
+    'monument': ("sealed burial mound","plundered burial mound",
                   "[common_monuments]", "[ruin_description] [common_monuments]"),
-    'common_monuments': ('obelisk', 'statue of a[n] [person_types]','statue of a[n] [renown] [person_types]',"circle of standing stones","totem pole","wayshrine"),
-    'rare_monuments': (),
-    'building': ('[basic_buildings]','[milit_buildings]' ,'[religious_buildings]' ,'[production_buildings]' , '[int_buildings]' ,'[meeting_buildings]','[tomb_buildings]' ),
+    'common_monuments': ('obelisk', 'statue of a[n] [person_types]','statue of a[n] [renown] [person_types]',"circle of standing stones","totem pole","wayshrine", "[rare_monuments]"),
+    'rare_monuments': ("pyramid","group of faces, carved into a mountainside or cliff,","group of giant statues, carved out of a mountainside or cliff,","sealed catacomb", "plundered catacomb", "plundered temple","circle of standing stones"),
+    'building': ('[basic_buildings]','[milit_buildings]' ,'[religious_buildings]' ,'[production_buildings]' , '[int_buildings]' ,'[meeting_buildings]','[tomb_buildings]','[signpost]' ),
     'basic_buildings' : ("inn","house", "tower","wall","staircase","pillar","hut","building","fountain","well", "archway"),
     'milit_buildings' : ("fortress","watchtower","outpost", "keep", "castle","guard station"),
-    'religious_buildings' : ("temple","monastery","reliquary","battlefield", ),
+    'religious_buildings' : ("temple","monastery","reliquary" ),
     'tomb_buildings' : ("tomb","crypt","necropolis","memorial stone","graveyard"),
     'production_buildings' : ("quarry","orchard","hunting stand", "mine","farm","windmill","garden", "hunter/fisherman's shack"),
     'int_buildings' : ("[int_location] of a[renown] [intellectual]","[int_location]","artificer’s factory","alchemist's lab"),
     'meeting_buildings' : ("private meeting house for a [faction]","private meeting house for a[renown] [faction]"),
-} #to do: cairn
+    'signpost': ()
+} #to do: cairn, battlefield
 
-print(text_construct(ruindict))
+print(text_construct(structuredict))
 
 
-def monument():
-    writing_options = ["a warning","a blessing","historical lore","a memorial","religious iconography","a holy symbol","an unholy symbol","arcane symbols","ancient graffiti", "a riddle"]
-    surrounding_options = ["fresh flowers","a strange smell", "unnatural mist"]
-    options = [f"surrounded by {random.choice(surrounding_options)}", f"etched with {random.choice(writing_options)}"]
-    location_options = ("up ahead","just off the path","visible nearby","a short walk away from the path","a half-mile off the path")
-    if random.random() > 0.95:
-        chosen = random.choice(("sealed pyramid", "plundered pyramid","group of faces, carved into a mountainside or cliff,","group of giant statues, carved out of a mountainside or cliff,","sealed tomb", "plundered tomb","sealed catacomb", "plundered catacomb", "plundered temple","circle of standing stones"))
-    else:
-        chosen = random.choice(())
-    if "aeiou".find(chosen[0]) == -1:
-        state_text = "A"
-    else:
-        state_text = "An"
-    if random.random() < 0.5:
-        post_text = f", {random.choice(options)}, is {random.choice(location_options)}."
-    else:
-        post_text = f" is {random.choice(location_options)}."
-    return (f"{state_text} {chosen}{post_text}", "monument",)
